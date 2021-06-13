f21fa9443a41f310e61ba2c39832b41eb467807d
manzeralam <manzeralam@manzers-air.lan1>
2021-06-13 11:14:14 -0400

    First Commit......

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+
+public class Test1 {
+	public static void main (String[] args) {
+		String hungey = "Yes ";
+		
+		
+		if (hungey == "yes")
+			System.out.println("eat something.....");
+		
+		else {
+			System.out.println("Do woekin.......");
+		}
+			
+	}
+
+}
